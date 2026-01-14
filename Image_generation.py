1f1f6cbc9bb9e03c18f1abe186463282f191fe40
Giuseppe Adinolfi <104144179+adinoolfi@users.noreply.github.com>
2026-01-14 12:39:57 +0100

    Delete Image_generation.py

--- Image_generation.py
+++
@@ -1,9 +0,0 @@
-from diffusers import AutoPipelineForText2Image 
-import torch
-import cv2
-
-pipeline= AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype= torch.float16).to("cuda")
-pipeline.load_lora_weights(r"C:\Users\PNP\Desktop\Nappi\ModelliLora\checkpoint-5000", weight_name="pytorch_lora_weights.safetensors")
-image= pipeline("a woman in white dress").images[0]
-
-im1=image.save("generazione.jpg")
